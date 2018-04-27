d2573ab53aae25b676864dcbe31ed61e70c41384
caggarw <chetna.aggarwal@monsanto.com>
2018-04-27 14:25:18 -0500

    new files

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-var env = require('process').env
-var axios = require('axios')
-var CronJob = require('cron').CronJob;
+const env = require('process').env
+const axios = require('axios')
+const CronJob = require('cron').CronJob
+const aws = require('aws-sdk')
 
-var url = env.SLACK_INCOMING_WEBHOOK_BOT_TESTING
+const url = env.SLACK_INCOMING_WEBHOOK_BOT_TESTING
 
-var body = {
-	text: 'Hello!',
-	username: 'Chetna'
+const s3 = new aws.S3({
+    accessKeyId: env.BOT_AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.BOT_AWS_SECRET_ACCESS_KEY
+})
+
+const params = {
+    Bucket: env.S3_BUCKET_NAME,
+    Key: env.S3_FILE_PATH
 }
 
+function getBirthdayList() {
+    s3.getObject(params, (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log("Found:", JSON.parse(data.Body.toString()))
+        }
+    })
+}
 
-var job = new CronJob('00 16 21 * * 1-5', function() {
-  /*
-   * Runs every weekday (Monday through Friday)
-   * at 11:30:00 AM. It does not run on Saturday
-   * or Sunday.
-   */
-	
+const body = {
+    text: 'Hello!',
+    username: 'Chetna'
+};
+
+
+let job = new CronJob('00 16 15 * * 1-5', function () {
+    getBirthdayList();
 	axios.post(url, body).then((response) => console.log(response)).catch((err) => console.log(err))
   }, function () {
     /* This function is executed when the job stops */
